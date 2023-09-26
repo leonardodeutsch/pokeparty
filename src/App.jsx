be1c2c000cc63b0343b7d7e334194be3c94d9e42
Leo <deutschleonardo@gmail.com>
2023-09-26 16:03:11 -0500

    implemented simple search bar to filter through pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import pokemons from "./utilities/pokemons.js";
 
 const App = () => {
   const [pokemonSearched, setPokemonSearched] = useState('');
+  const [filteredPokemon, setFilteredPokemon] = useState(pokemons);
+
+  useEffect(() => {
+    setFilteredPokemon(pokemons.filter(pokemon => pokemon.name.english.toLowerCase().includes(pokemonSearched.toLowerCase())));
+  }, [pokemonSearched]);
 
   const searchPokemon = (e) => {
     setPokemonSearched(e.target.value);
@@ -16,7 +21,7 @@ const App = () => {
       </h1>
       <input type="text" className="search-pokemon" onChange={searchPokemon}/>
       <div className="pokemon-list">
-        {pokemons.map(pokemon => (
+        {filteredPokemon.map(pokemon => (
           <div key={pokemon.id}>{pokemon.id}. {pokemon.name.english}</div>
         ))}
       </div>
