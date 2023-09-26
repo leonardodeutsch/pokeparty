5527db5abbc2471b1a0e47c020f84301012f01a1
Leo <deutschleonardo@gmail.com>
2023-09-26 15:57:00 -0500

    rendered a basic list of the data in pokemons.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import pokemons from "./utilities/pokemons.js";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
+const App = () => {
+  const [pokemonSearched, setPokemonSearched] = useState('');
+
+  const searchPokemon = (e) => {
+    setPokemonSearched(e.target.value);
   }
+
+  return (
+    <div>
+      <h1>
+        PokeParty
+      </h1>
+      <input type="text" className="search-pokemon" onChange={searchPokemon}/>
+      <div className="pokemon-list">
+        {pokemons.map(pokemon => (
+          <div key={pokemon.id}>{pokemon.id}. {pokemon.name.english}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
