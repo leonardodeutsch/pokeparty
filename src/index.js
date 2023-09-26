3d26e534b7737744c6dc8b1c682eabaa1b103a99
Leo <deutschleonardo@gmail.com>
2023-09-26 14:57:59 -0500

    wrapped the rendered react app with the BrowserRouter component from react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App/>, mountNode);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
